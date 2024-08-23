02be3a4351c63b1988fbb31d4bc7c6c0c0c8e3d2
MariaEduardaKnupp <maria_ratto@estudante.sesisenai.org.br>
2024-08-23 17:16:13 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <h1>Bob Esponja</h1>
+       <h2>Patrick Estrela</h2>
+       <p>hihihihihihih </p>
+    </>
+  )
+}
+
+export default App
