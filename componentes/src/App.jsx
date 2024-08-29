890160d207d32ca5bee415569051189af2a5d011
MariaEduardaKnupp <maria_ratto@estudante.sesisenai.org.br>
2024-08-29 17:20:47 -0300

    add criação de componentes

---
+++ componentes/src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Atalhos do VS Code</h1>
+      <h2>Coisas legais para facilitar tua vida</h2>
+      <p>
+        comando para o emmet da ul>li*6
+      </p>
+      <ul>
+        <li>Atalho: alt + z para quebra automática de linha</li>
+        <li>Atalho: alt + clique esquerdo para adicionar novos cursores na tela</li>
+        <li>Atalho: ctrl+shift+k para excluir uma linha inteira</li>
+        <li>Atalho: ctrl+; para comentar/descomentar</li>
+        <li>Atalho: windows+. abre teclado de emojis</li>
+        <li>Atalho: alt+↕ para mover uma linha inteira</li>
+        <li>Atalho: ctrl+j ou ctrl+' abre o terminal  </li>
+        <li>Atalho: alt+shift+↕ copia a linha atual ou blocos selecionados</li>
+
+      </ul>
+      <p>para esfregar vários cursores pela tela (digitar várias coisas ao mesmo tempo): Clica com a rodinha 
+      e e esfrega elapela tela
+      </p>
+
+      <p>Extensões:</p>
+      <ul>
+        <li>ES7 React/Redux/GraphQL/React-Native snippets</li>
+        <li>Auto Rename Tag</li>
+        <li>Color highlight</li>
+        <li>Power Mode</li>
+        <li>VSCode Pets</li>
+        <li></li>
+      </ul>
+    </>
+  )
+}
+
+export default App
