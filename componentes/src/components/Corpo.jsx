aaeb8d343607bb3fb78d96d0b839611865cfd2fd
MariaEduardaKnupp <maria_ratto@estudante.sesisenai.org.br>
2024-08-30 17:07:44 -0300

    Atualização de cores e add img

---
+++ componentes/src/components/Corpo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Corpo() {
+  return (
+    <div>
+      <h2>Eu sou o corpito da página. Sou muito mais que só um corpinho bonitinho...</h2>
+      <img src="./images/furia da luz.webp" alt="" />
+    </div>
+  )
+}
+
+export default Corpo
---
+++ componentesvdd/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Cabecalho from './components/Cabecalho'
+import Corpo from './components/Corpo'
+import Links from './components/Links'
+
+function App() {
+
+  return (
+    <>
+     <Cabecalho />
+     <Links />
+     <Corpo />
+    </>
+  )
+}
+
+export default App
